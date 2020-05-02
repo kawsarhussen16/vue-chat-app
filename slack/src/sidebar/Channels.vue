54bfc9b4e2514e53f6cd7c318a0a03b19a3c9a60
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-02 03:18:15 -0400

    popup modal added for add channel

---
+++ slack/src/sidebar/Channels.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <button @click="openModal" class="btn btn-outline-primary">
+      Add Channel
+    </button>
+    <div class="modal fade" id="channelModal">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-dark">Modal title</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <input
+                  type="text"
+                  id="new-channel"
+                  name="new-channel"
+                  class="form-control"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button type="button" class="btn btn-primary">Add Channel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+require("bootstrap/js/dist/modal");
+export default {
+  name: "channels",
+  methods: {
+    openModal() {
+      $("#channelModal")
+        .appendTo("body")
+        .modal("show");
+    },
+  },
+};
+</script>
--- slack/src/sidebar/Sidebar.vue
+++ slack/src/sidebar/Sidebar.vue
@@ -6,16 +6,18 @@
       <img class="rounded-circle mr-2" height="30" :src="currentUser.photoURL" />
       <span class="taxt-light mr-2">{{currentUser.displayName}}</span>
     </span>
-    <hr style="border: 1px solid #333;" />
     <button @click="logout" class="btn btn-outline-light">Logout</button>
+    <hr style="border: 1px solid #333;" />
+    <Channels></Channels>
   </div>
 </template>
 <script>
 import firebase from "firebase/app";
 import { mapGetters } from "vuex";
-
+import Channels from "./Channels";
 export default {
   name: "sidebar",
+  components: { Channels },
   computed: {
     ...mapGetters(["currentUser"])
   },
